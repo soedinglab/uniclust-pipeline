33933013c0011f927ba768dbade3ee33d4473fbd
Milot Mirdita <milot@mirdita.de>
2017-02-16 14:08:32 +0100

    Insert error, user should chose the right download url

--- download_uniprot.sh
+++ download_uniprot.sh
@@ -3,6 +3,9 @@ source ./paths.sh
 
 mkdir -p "uniprot/${RELEASE}"
 
+echo "Please insert the base url for the chosen release below and comment this line and the next."
+exit 1
+
 wget ftp://ftp.expasy.org/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz -O uniprot/${RELEASE}/uniprot_sprot.fasta.gz 
 wget ftp://ftp.expasy.org/databases/uniprot/current_release/knowledgebase/complete/uniprot_trembl.fasta.gz -O uniprot/${RELEASE}/uniprot_trembl.fasta.gz 
 
